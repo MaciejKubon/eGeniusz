e119ae6522033f21663894ced3a4a5b2b083c10f
Maciej Kubon <maciejk2233@gmail.com>
2025-04-12 19:36:50 +0200

    create loginPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './page/login/login.component';
 
 export const routes: Routes = [
-    
+    {path:'login',component:LoginComponent}
 ];
