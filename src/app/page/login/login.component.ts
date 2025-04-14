953d4e938c1c55883afcf56223c5e19811c565b8
Maciej Kubon <maciejk2233@gmail.com>
2025-04-14 23:20:59 +0200

    create registration page

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NotificationService } from '../../services/notification/notification.service';
 import { LoginFormComponent } from '../../component/forms/login-form/login-form.component';
 
 @Component({
@@ -10,12 +9,5 @@ import { LoginFormComponent } from '../../component/forms/login-form/login-form.
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  constructor(
-    private notificationService: NotificationService
-  ) {}
-
-  turnon(){
-    this.notificationService.showSuccess('Dane zapisane pomyślnie!');
-  }
 
 }
