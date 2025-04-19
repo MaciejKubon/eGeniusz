8ed5cf446a360558e2045cd8bc262e37c421b24f
Maciej Kubon <maciejk2233@gmail.com>
2025-04-19 22:31:25 +0200

    add nav, logout, close, menu buttons

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { NavButtonComponent } from '../buttons/nav-button/nav-button.component';
+import { linkButton } from '../../interfaces/buttonInterfaces';
+import { LogoutButtonComponent } from '../buttons/logout-button/logout-button.component';
+import { MenuButtonComponent } from '../buttons/menu-button/menu-button.component';
+import { CloseButtonComponent } from '../buttons/close-button/close-button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NavButtonComponent,
+    LogoutButtonComponent,
+    MenuButtonComponent,
+    CloseButtonComponent,
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  navigation: linkButton = { path: 'aaa', text: 'aaa' };
+  showMenu(action: boolean) {
+    console.log(action);
+  }
+  hideMenu(action: boolean) {
+    console.log(action);
+  }
 }
